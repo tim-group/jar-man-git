481ed6d883fc3dd9aba5387b49bd729e366d4aeb
Tom Denley <tom.denley@timgroup.com>
2016-11-23 19:24:01 +0000

    tomd: get sbt plugin working as an autoplugin

--- sbt-jarmangit/src/main/scala/com/timgroup/sbtjarmangit/SbtJarManGit.scala
+++ sbt-jarmangit/src/main/scala/com/timgroup/sbtjarmangit/SbtJarManGit.scala
@@ -1,14 +1,18 @@
 package com.timgroup
 package sbtjarmangit
 
-import sbt._
-import Keys._
-
-import org.eclipse.jgit.storage.file.FileRepositoryBuilder
-import org.eclipse.jgit.lib._
 import org.eclipse.jgit.api.Git
+import org.eclipse.jgit.lib._
+import org.eclipse.jgit.storage.file.FileRepositoryBuilder
+import sbt.Keys._
+import sbt._
 
 object SbtJarManGit extends AutoPlugin {
+  override def trigger = allRequirements
+
+  override lazy val projectSettings: Seq[sbt.Def.Setting[_]] = Seq(
+    packageOptions in (Compile, packageBin) += Package.ManifestAttributes(repoInfo: _*)
+  )
 
   def repoInfo: List[(String, String)] = {
     val repository = (new FileRepositoryBuilder).readEnvironment.findGitDir.build
@@ -20,12 +24,10 @@ object SbtJarManGit extends AutoPlugin {
     val isClean = new Git(repository).status.call.isClean.toString
 
     List(
-       "Git-Origin" -> origin,
-       "Git-Branch" -> branch,
-       "Git-Head-Rev" -> head,
-       "Git-Repo-Is-Clean" -> isClean
+      "Git-Origin" -> origin,
+      "Git-Branch" -> branch,
+      "Git-Head-Rev" -> head,
+      "Git-Repo-Is-Clean" -> isClean
     )
   }
-
-  val jarManGitSettings = Seq(packageOptions += { Package.ManifestAttributes(repoInfo: _*) })
 }
