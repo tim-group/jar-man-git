474afe0f64e4c9ad4e5269f40f6244850f578f1b
Tom Denley <tom.denley@timgroup.com>
2016-11-24 11:29:48 +0000

    tomd: put the real git bits in using the gradle plugin

--- gradle-jarmangit/src/main/groovy/com/timgroup/gradlejarmangit/GradleJarManGitPlugin.groovy
+++ gradle-jarmangit/src/main/groovy/com/timgroup/gradlejarmangit/GradleJarManGitPlugin.groovy
@@ -1,41 +1,34 @@
 package com.timgroup.gradlejarmangit
 
+import org.eclipse.jgit.api.Git
+import org.eclipse.jgit.lib.Constants
+import org.eclipse.jgit.lib.ObjectId
+import org.eclipse.jgit.lib.Repository
+import org.eclipse.jgit.storage.file.FileRepositoryBuilder
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.tasks.bundling.Jar
 
-import org.eclipse.jgit.api.Git
-import org.eclipse.jgit.lib.*
-import org.eclipse.jgit.storage.file.FileRepositoryBuilder
+final class GradleJarManGitPlugin implements Plugin<Project> {
 
-public final class GradleJarManGitPlugin implements Plugin<Project> {
+  static Map<String, String> repoInfo() {
+    Repository repository = new FileRepositoryBuilder().readEnvironment().findGitDir().build()
 
-  Map repoInfo() {
-    repository = (new FileRepositoryBuilder()).readEnvironment.findGitDir.build
+    String origin = repository.getConfig().getString("remote", "origin", "url")
+    String head = ObjectId.toString(repository.findRef(Constants.HEAD).getObjectId())
+    String branch = repository.getBranch()
+    String isClean = new Git(repository).status().call().isClean().toString()
 
-    origin = repository.getConfig.getString("remote", "origin", "url")
-    head = ObjectId.toString(repository.findRef(Constants.HEAD).getObjectId)
-    branch = repository.getBranch
-
-    isClean = new Git(repository).status.call.isClean.toString
-
-    Map(
+    [
       "Git-Origin": origin,
       "Git-Branch": branch,
       "Git-Head-Rev": head,
       "Git-Repo-Is-Clean": isClean
-    )
+    ]
   }
 
   @Override
   void apply(Project project) {
-    project.tasks.withType(Jar) { jar ->
-      jar.manifest.attributes(
-          "Git-Origin": "a",
-          "Git-Branch": "b",
-          "Git-Head-Rev": "c",
-          "Git-Repo-Is-Clean": "d"
-      )
-    }
+    project.tasks.withType(Jar) { jar -> jar.manifest.attributes(repoInfo()) }
   }
 }
--- gradle-jarmangit/src/test/groovy/com/timgroup/gradlejarmangit/GradleJarManGitPluginTest.groovy
+++ gradle-jarmangit/src/test/groovy/com/timgroup/gradlejarmangit/GradleJarManGitPluginTest.groovy
@@ -1,27 +1,27 @@
 package com.timgroup.gradlejarmangit
 
-import org.gradle.api.artifacts.Dependency
+import org.gradle.api.Project
 import org.gradle.api.internal.artifacts.dependencies.DefaultExternalModuleDependency
-import org.gradle.api.internal.artifacts.dependencies.DefaultProjectDependency
-import org.junit.Test
 import org.gradle.testfixtures.ProjectBuilder
-import org.gradle.api.Project
-import static org.junit.Assert.*
+import org.junit.Test
 
-import org.gradle.api.DefaultTask
-import org.gradle.api.Plugin
-import org.gradle.api.Project
-import org.gradle.api.internal.project.DefaultProject
-import org.gradle.api.tasks.TaskAction
-
-import org.junit.Rule
-
+import static org.hamcrest.CoreMatchers.*
+import static org.junit.Assert.assertThat
 
 class GradleJarManGitPluginTest {
 
     @Test
-    public void resolvesDependenciesFromAutobumpFile() {
-        def Project project = ProjectBuilder.builder().build()
+    void generatesRepoInfo() {
+        Map<String, String> repoInfo = GradleJarManGitPlugin.repoInfo()
+        assertThat(repoInfo.get("Git-Origin"), containsString("jar-man-git"))
+        assertThat(repoInfo.get("Git-Branch"), is(equalTo("master")))
+        assertThat(repoInfo.get("Git-Head-Rev").length(), is(equalTo(40)))
+        assertThat(repoInfo.get("Git-Repo-Is-Clean"), is(either(equalTo("true")) | equalTo("false")))
+    }
+
+    @Test
+    void addsMetadataToManifest() {
+        Project project = ProjectBuilder.builder().build()
         project.apply plugin: 'java-base'
         project.apply plugin: 'jarmangit'
 
