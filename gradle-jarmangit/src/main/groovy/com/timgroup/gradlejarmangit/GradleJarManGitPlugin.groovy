e9ad47d5cc5bf5b8e459fe134fa3a547bb3beff8
Tom Denley <tom.denley@timgroup.com>
2016-11-24 00:10:32 +0000

    tomd: basic gradle plugin structure

--- gradle-jarmangit/src/main/groovy/com/timgroup/gradlejarmangit/GradleJarManGitPlugin.groovy
+++ gradle-jarmangit/src/main/groovy/com/timgroup/gradlejarmangit/GradleJarManGitPlugin.groovy
@@ -2,30 +2,18 @@ package com.timgroup.gradlejarmangit
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.internal.file.IdentityFileResolver
-import org.gradle.api.java.archives.internal.DefaultManifest
 import org.gradle.api.tasks.bundling.Jar
 
 public final class GradleJarManGitPlugin implements Plugin<Project> {
   @Override
   void apply(Project project) {
-    project.tasks.withType(Jar) {
-      manifest = new DefaultManifest(new IdentityFileResolver()) {
-          @Override
-          DefaultManifest attributes(Map<String, ?> attributes) {
-              return super.attributes(attributes)
-          }
-//        attributes(
-//            (java.util.jar.Attributes.Name.IMPLEMENTATION_TITLE.toString()): (project.hasProperty('BF_PROJECTNAME') ? project.getProperty('BF_PROJECTNAME') : "${ -> project.someCustomExtensionObject.fieldInCustomExtensionObject}"),
-//            (java.util.jar.Attributes.Name.IMPLEMENTATION_VERSION.toString()): project.hasProperty('BF_TAG') ? project.getProperty('BF_TAG') : 'UNCONTROLLED ARTIFACT',
-//            (java.util.jar.Attributes.Name.IMPLEMENTATION_VENDOR.toString()): 'MyCompany',
-//            'Created-By': System.getProperty('java.version') + ' (' + System.getProperty('java.vendor') + ')',
-//            'Built-With': "gradle-${project.getGradle().getGradleVersion()}, groovy-${GroovySystem.getVersion()}",
-//            'Build-Time': project.hasProperty('Current_Date') ? project.getProperty('Current_Date') : 'NOT FOR PRODUCTION USE',
-//            'Built-By': project.hasProperty('BF_USER') ? project.getProperty('BF_USER') : System.getProperty('user.name'),
-//            'Built-On': "${InetAddress.localHost.hostName}/${InetAddress.localHost.hostAddress}"
-//            )
-      }
+    project.tasks.withType(Jar) { jar ->
+      jar.manifest.attributes(
+          "Git-Origin": "a",
+          "Git-Branch": "b",
+          "Git-Head-Rev": "c",
+          "Git-Repo-Is-Clean": "d"
+      )
     }
   }
 }
