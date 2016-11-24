9512aaacfde53ce7b29146c198b6ef472c557e6e
Tom Denley <tom.denley@timgroup.com>
2016-11-24 14:16:57 +0000

    tomd: improve scala test

--- sbt-jarmangit/src/test/scala/com/timgroup/sbtjarmangit/SbtJarManGitSuite.scala
+++ sbt-jarmangit/src/test/scala/com/timgroup/sbtjarmangit/SbtJarManGitSuite.scala
@@ -16,10 +16,10 @@ class SbtJarManGitSuite extends FunSuite {
     ))
 
     val values = repoInfo.map(t => t._2)
-//    assert(values(0) == "git@git.net.local:sbt-jarmangit")
+    assert(values(0).contains("jar-man-git"))
     assert(values(1) == "master")
     assert(values(2).length == 40)
-//    assert(values(3) == "true")
+    assert(values(3) == "true" || values(3) == "false")
   }
 
 }
