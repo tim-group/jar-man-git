c8f45c5038daea4555f90a1ddb4ca792b87725eb
Tom Denley <tom.denley@timgroup.com>
2016-11-25 13:47:32 +0000

    tomd: indent consistently

--- gradle-jarmangit/src/main/groovy/com/timgroup/gradlejarmangit/GradleJarManGitPlugin.groovy
+++ gradle-jarmangit/src/main/groovy/com/timgroup/gradlejarmangit/GradleJarManGitPlugin.groovy
@@ -15,42 +15,41 @@ import java.lang.reflect.Method
 
 final class GradleJarManGitPlugin implements Plugin<Project> {
 
-  static Map<String, String> repoInfo() {
-    Repository repository = new FileRepositoryBuilder().readEnvironment().findGitDir().build()
+    static Map<String, String> repoInfo() {
+        Repository repository = new FileRepositoryBuilder().readEnvironment().findGitDir().build()
 
-    String origin = repository.getConfig().getString("remote", "origin", "url")
-    String head = ObjectId.toString(repository.findRef(Constants.HEAD).getObjectId())
-    String branch = repository.getBranch()
-    String isClean = new Git(repository).status().call().isClean().toString()
+        String origin = repository.getConfig().getString("remote", "origin", "url")
+        String head = ObjectId.toString(repository.findRef(Constants.HEAD).getObjectId())
+        String branch = repository.getBranch()
+        String isClean = new Git(repository).status().call().isClean().toString()
 
-    [
-      "Git-Origin": origin,
-      "Git-Branch": branch,
-      "Git-Head-Rev": head,
-      "Git-Repo-Is-Clean": isClean
-    ]
-  }
-
-  @Override
-  void apply(Project project) {
-    project.tasks.withType(Jar) { jar -> jar.manifest.attributes(repoInfo()) }
-
-    Upload uploadArchives = (Upload)project.getTasks().withType(Upload.class).findByName("uploadArchives")
-    if (uploadArchives != null) {
-        def info = repoInfo()
-
-        uploadArchives.repositories.mavenDeployer { PomFilterContainer deployer ->
-            def model = deployer.getPom().getModel()
-
-            Method setScmMethod = model.getClass().getMethods().find { method -> (method.name == "setScm") }
-            Class<?> scmType = setScmMethod.parameterTypes[0]
-
-            def scm = scmType.newInstance()
-            scmType.getMethod("setTag", String.class).invoke(scm, info.get("Git-Head-Rev"))
-            scmType.getMethod("setUrl", String.class).invoke(scm, info.get("Git-Origin"))
-            setScmMethod.invoke(model, scm)
-        }
+        [
+          "Git-Origin": origin,
+          "Git-Branch": branch,
+          "Git-Head-Rev": head,
+          "Git-Repo-Is-Clean": isClean
+        ]
     }
 
-  }
+    @Override
+    void apply(Project project) {
+        project.tasks.withType(Jar) { jar -> jar.manifest.attributes(repoInfo()) }
+
+        Upload uploadArchives = (Upload)project.getTasks().withType(Upload.class).findByName("uploadArchives")
+        if (uploadArchives != null) {
+            def info = repoInfo()
+
+            uploadArchives.repositories.mavenDeployer { PomFilterContainer deployer ->
+                def model = deployer.getPom().getModel()
+
+                Method setScmMethod = model.getClass().getMethods().find { method -> (method.name == "setScm") }
+                Class<?> scmType = setScmMethod.parameterTypes[0]
+
+                def scm = scmType.newInstance()
+                scmType.getMethod("setTag", String.class).invoke(scm, info.get("Git-Head-Rev"))
+                scmType.getMethod("setUrl", String.class).invoke(scm, info.get("Git-Origin"))
+                setScmMethod.invoke(model, scm)
+            }
+        }
+    }
 }
