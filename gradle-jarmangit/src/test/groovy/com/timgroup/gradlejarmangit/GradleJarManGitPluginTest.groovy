798a270df4b25ff1d0c0b6ef1e65781f4afc90c4
Steve Haslam <steve.haslam@timgroup.com>
2017-10-03 16:48:27 +0100

    SteveH: test specifying properties using -P switch to GradleRunner
    This closely emulates how we expect these to be passed.
    
    Fold in tests that simply call repoInfo directly

--- gradle-jarmangit/src/test/groovy/com/timgroup/gradlejarmangit/GradleJarManGitPluginTest.groovy
+++ gradle-jarmangit/src/test/groovy/com/timgroup/gradlejarmangit/GradleJarManGitPluginTest.groovy
@@ -1,8 +1,5 @@
 package com.timgroup.gradlejarmangit
 
-import org.gradle.api.Project
-import org.gradle.api.plugins.ExtraPropertiesExtension
-import org.gradle.testfixtures.ProjectBuilder
 import org.gradle.testkit.runner.GradleRunner
 import org.gradle.testkit.runner.TaskOutcome
 import org.hamcrest.Description
@@ -32,37 +29,6 @@ class GradleJarManGitPluginTest {
     }
 
     @Test
-    void "generates repo info"() {
-        Project project = ProjectBuilder.builder().build()
-        project.apply plugin: 'java-base'
-
-        def extraProperties = project.getExtensions().getByType(ExtraPropertiesExtension)
-
-        extraProperties.set("jarmangit.origin", "git://git.example.com/repo")
-        extraProperties.set("jarmangit.revision", "4b825dc642cb6eb9a060e54bf8d69288fbee4904")
-        extraProperties.set("jarmangit.branch", "master")
-        extraProperties.set("jarmangit.dirty", "false")
-
-        Map<String, String> repoInfo = GradleJarManGitPlugin.repoInfo(project)
-        assertThat(repoInfo.get("Git-Origin"), is(equalTo("git://git.example.com/repo")))
-        assertThat(repoInfo.get("Git-Branch"), is(equalTo("master")))
-        assertThat(repoInfo.get("Git-Head-Rev"), is(equalTo("4b825dc642cb6eb9a060e54bf8d69288fbee4904")))
-        assertThat(repoInfo.get("Git-Repo-Is-Clean"), is(equalTo("true")))
-    }
-
-    @Test
-    void "generates repo info from project properties"() {
-        Project project = ProjectBuilder.builder().build()
-        project.apply plugin: 'java-base'
-
-        Map<String, String> repoInfo = GradleJarManGitPlugin.repoInfo(project)
-        assertThat(repoInfo.get("Git-Origin"), containsString("jar-man-git"))
-        assertThat(repoInfo.get("Git-Branch"), is(equalTo("master")))
-        assertThat(repoInfo.get("Git-Head-Rev").length(), is(equalTo(40)))
-        assertThat(repoInfo.get("Git-Repo-Is-Clean"), is(either(equalTo("true")) | equalTo("false")))
-    }
-
-    @Test
     void "adds metadata to manifest"() {
         settingsFile << """
 rootProject.name = 'testee'
@@ -82,8 +48,46 @@ plugins {
 
         assertThat(result.task(":jar").outcome, is(equalTo(TaskOutcome.SUCCESS)))
 
+        // yuk, this is taking settings from the local checkout of jar-man-git itself
+        // this is arguably a bug, the plugin should only look at where the project lives
         new JarFile(testProjectDir.root.toPath().resolve("build/libs/testee.jar").toFile()).withCloseable { jarFile ->
-            assertThat(jarFile.manifest, containsAttribute("Git-Branch", equalTo("master")))
+            assertThat(jarFile.manifest, containsAttribute("Git-Head-Rev", is(sha1Hash())))
+            assertThat(jarFile.manifest, containsAttribute("Git-Branch", is(equalTo("master"))))
+            assertThat(jarFile.manifest, containsAttribute("Git-Origin", containsString("jar-man-git")))
+            assertThat(jarFile.manifest, containsAttribute("Git-Repo-Is-Clean", is(either(equalTo("true")) | equalTo("false"))))
+        }
+    }
+
+    @Test
+    void "adds metadata from project properties to manifest"() {
+        settingsFile << """
+rootProject.name = 'testee'
+"""
+        buildFile << """
+plugins {
+  id 'com.timgroup.jarmangit'
+  id 'java'
+}
+"""
+
+        def result = GradleRunner.create()
+            .withProjectDir(testProjectDir.root)
+            .withArguments(
+                "-Pjarmangit.revision=4b825dc642cb6eb9a060e54bf8d69288fbee4904",
+                "-Pjarmangit.branch=some-branch",
+                "-Pjarmangit.origin=git://git.example.com/repo.git",
+                "-Pjarmangit.dirty=false",
+                "assemble")
+            .withPluginClasspath()
+            .build()
+
+        assertThat(result.task(":jar").outcome, is(equalTo(TaskOutcome.SUCCESS)))
+
+        new JarFile(testProjectDir.root.toPath().resolve("build/libs/testee.jar").toFile()).withCloseable { jarFile ->
+            assertThat(jarFile.manifest, containsAttribute("Git-Head-Rev", equalTo("4b825dc642cb6eb9a060e54bf8d69288fbee4904")))
+            assertThat(jarFile.manifest, containsAttribute("Git-Branch", equalTo("some-branch")))
+            assertThat(jarFile.manifest, containsAttribute("Git-Origin", equalTo("git://git.example.com/repo.git")))
+            assertThat(jarFile.manifest, containsAttribute("Git-Repo-Is-Clean", equalTo("true")))
         }
     }
 
@@ -203,6 +207,22 @@ plugins {
             @Override
             void describeTo(Description description) {
                 description.appendText("has attribute ").appendValue(name).appendText(" ").appendDescriptionOf(valueMatcher)
+            }
+        }
+    }
+
+    static def sha1Hash() {
+        return new TypeSafeDiagnosingMatcher<String>() {
+            @Override
+            protected boolean matchesSafely(String item, Description mismatchDescription) {
+                def hexDigits = "0123456789abcdef"
+                mismatchDescription.appendText("was ").appendValue(item)
+                return item.length() == 40 && !item.chars.any { hexDigits.indexOf((int) it.charValue()) < 0 }
+            }
+
+            @Override
+            void describeTo(Description description) {
+                description.appendText("SHA-1 hash")
             }
         }
     }
